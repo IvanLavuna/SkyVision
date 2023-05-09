61597a9e6101d760e12b1ee2432895ddf1210b03
Ivan Manchur <ivan.manchur47@gmail.com>
2023-05-09 22:35:22 +0300

    Decomposed code into 2 functions

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from djitellopy import tello
 import cv2
 
-me = tello.Tello()
-me.connect()
-print(me.get_battery())
+my_tello = tello.Tello()
 
-me.streamon()
+def processing_loop():
+    while True:
+        img = my_tello.get_frame_read().frame
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
 
-while True:
-    img = me.get_frame_read().frame
-    cv2.imshow ("Image", img)
-    cv2.waitKey(1)
+
+def init_tello():
+    my_tello.connect()
+    print("[info] battery", my_tello.get_battery())
+    my_tello.streamon()
+
+
+if __name__ == '__main__':
+    init_tello()
+    processing_loop()
