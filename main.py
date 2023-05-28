e4e43445115193708dedddb15a3a06c41b51996b
Ivan Manchur <ivan.manchur47@gmail.com>
2023-05-28 17:54:57 +0300

    Cleanup

--- main.py
+++ main.py
@@ -14,40 +14,6 @@ env = Environment(my_tello)
 agent = Agent(env)
 images_path = "/home/lavuna47/Projects/SkyVision/images"
 
-def manual_drone_control_step(drone: tello.Tello):
-    lr, fb, ud, yv = 0, 0, 0, 0
-    speed = 30
-
-    if kp.get_key("LEFT"):
-        lr = -speed
-    elif kp.get_key("RIGHT"):
-        lr = speed
-
-    if kp.get_key("UP"):
-        fb = speed
-    elif kp.get_key("DOWN"):
-        fb = -speed
-
-    if kp.get_key("w"):
-        ud = 2*speed
-    elif kp.get_key("s"):
-        ud = -speed
-
-    if kp.get_key("a"):
-        yv = -speed
-    elif kp.get_key("d"):
-        yv = speed
-
-    if kp.get_key('q'):
-        drone.land()
-
-    if kp.get_key('e'):
-        drone.takeoff()
-
-    # give chance for user to interfere
-    if lr != 0 or fb != 0 or ud != 0 or yv != 0:
-        drone.send_rc_control(lr, fb, ud, yv)
-
 
 def processing_loop():
     dt = 0.05  # sec
@@ -60,8 +26,9 @@ def processing_loop():
         # 2. updating env
         env.AddImage(img)
 
-        # 3. drone manual control
-        manual_drone_control_step(my_tello)
+        # 3. safety measures
+        if kp.get_key('q'):
+            my_tello.land()
 
         # 4. agent control
         agent.next_move()
